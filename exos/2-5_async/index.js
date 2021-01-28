47c8a769fc4e06c48cb78778b6d230b077b1cf2d
eguilley <elsa.guilley@lpo.fr>
2021-01-28 17:40:53 +0100

    Jour 2

--- exos/2-5_async/index.js
+++ exos/2-5_async/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
+function getJson(url) {
+  return fetch(url)
+    .then(response => {
+      return response.json()
+    })
+}
+
 document.addEventListener(
   'DOMContentLoaded',
-  function () {
-    fetch(
-      'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json',
-    )
-      .then(function (resp) {
-        return resp.json();
-      })
-      .then(function (tweets) {
+  getJson('https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json')
+  // function () {
+  //   fetch(
+  //     'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json',
+  //   )
+  //     .then(function (resp) {
+  //       return resp.json();
+  //     })
+      .then(async function (tweets) {
         console.log('Le tableau de tweet', tweets);
 
         // Asynchrone
@@ -20,15 +28,37 @@ document.addEventListener(
 
             Utiliser setTimeout().
         */
+        const maPromesse = new Promise((res, rej) => {
+          setTimeout(() => {
+            const myNumber = Math.floor(Math.random() * (99 - 0 + 1) + 0)
+            if (myNumber % 2 === 0) res(myNumber)
+            else rej(myNumber)
+          }, 2000)
+        })
 
         console.log('--- Question 2 ---');
         /* Consommer la Promesse de 1) pour
             - afficher le texte du tweet correspondant au nombre renvoyé quand elle résoud
             - logguer un message d'erreur expliquant que le nombre est incorrect quand elle est rejetée
         */
+        maPromesse
+          .then(vp => {
+            console.log('Le nombre ' + vp + ' est correct')
+            console.log(tweets[vp].full_text)
+          })
+          .catch(error => {
+            console.log('Le nombre ' + error + ' est incorrect')
+          })
 
         console.log('--- Question 3 ---');
         // Même chose que la 2), mais avec async/await
+        try {
+          const vp = await maPromesse
+          console.log('Le nombre ' + vp + ' est correct')
+          console.log(tweets[vp].full_text)
+        } catch (error) {
+          console.log('Le nombre ' + error + ' est incorrect')
+        }
 
         console.log('--- Question 4 ---');
         /* Écrire une fonction getJson() qui prend une URL en entrée,
@@ -42,13 +72,45 @@ document.addEventListener(
         /* Créer une Promesse qui:
           - crée un nombre aléatoire (delay) en 0 et 5000
           - au bout de 2s, rejeter la Promesse avec le texte 'Trop long...'
-          - au bout de delay ms pour résoudre le nombre delay
+          - au bout de delay ms, résoudre le nombre delay
           - consommer la Promesse de sorte que si elle est rejetée, on recommence jusqu'à ce qu'elle résolve
         */
+        const nouvellePromesse = new Promise((res, rej) => {
+          const delay = Math.floor(Math.random() * (5000 - 0 + 1) + 0)
+          setTimeout(() => {
+            rej('Trop long...')
+          }, 2000)
+          setTimeout(() => {
+            res(delay)
+          }, delay)
+        })
+        
+        nouvellePromesse
+          .then(vp => {
+            console.log('Valeur promise : ' + vp)
+          })
+          .catch(error => {
+            console.error(error)
+          })
+        
+        // function startPromesse() {
+        //   nouvellePromesse
+        //   .then(vp => {
+        //     console.log('Valeur promise : ' + vp)
+        //     return vp
+        //   })
+        //   .catch(error => {
+        //     console.log('Start again')
+        //     return startPromesse()
+        //   })
+        // }
+        // startPromesse()
+
       })
       .catch(function (e) {
         console.error(e);
-      });
-  },
-  { once: true },
+      })
+  // },
+  // { once: true },
 );
+
--- src/main.js
+++ src/main.js
@@ -1,16 +1,66 @@
 'use strict';
 
+function createLi(tweet) {
+  const li = document.createElement('li')
+  li.textContent = tweet.full_text
+  return li
+}
+
+function createOl(tweets) {
+  const ol = document.createElement('ol')
+  const liTweets = tweets.map(createLi)
+  ol.append(...liTweets)
+  return ol
+}
+
+function listenerTrack(event) {
+  console.log('Position de la souris : ' + [event.clientX, event.clientY])
+}
+
+function createFrenchButton(label, selector, baseOl, specificOl) {
+  let filter = false
+  const button = document.createElement('button')
+  button.textContent = label
+  button.addEventListener(
+    selector,
+    function () {
+      filter = !filter
+      if (filter) {
+        baseOl.replaceWith(specificOl)
+      } else {
+        specificOl.replaceWith(baseOl)
+      }
+    }
+  )
+  return button
+}
+
+function getJson(url) {
+  return fetch(url)
+    .then(response => {
+      return response.json()
+    })
+}
+
 document.addEventListener(
   'DOMContentLoaded',
-  function () {
-    fetch(
-      'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json',
-    )
-      .then(function (resp) {
-        return resp.json();
-      })
-      .then(function (tweets) {
-        console.log('Le tableau de tweet', tweets);
+  Promise.all(
+    [
+      getJson('https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json'),
+      getJson('https://raw.githubusercontent.com/iOiurson/formation/correction/data/tweets2.json')
+    ]
+  )
+  // function () {
+  //   fetch(
+  //     'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json',
+  //   )
+  //     .then(function (resp) {
+  //       return resp.json();
+  //     })
+      .then(function (allTweets) {
+        console.log('Le tableau de tweet', allTweets);
+        const tweets = [...allTweets[0], ...allTweets[1]]
+        console.log(tweets)
 
         // ### Projet Touitter ###
         // Attention: toucher au DOM coûte cher, utiliser le moins possible les APIs du DOM
@@ -18,8 +68,37 @@ document.addEventListener(
         // [1] créer une fonction createLi(), qui pour un tweet en entrée, crée et retourne un <li> contenant le texte du tweet
 
         // [2] créer et ajouter un <ol> à la page, puis y ajouter les <li> de tweets en utilisant [1]
-
+        const ol = createOl(tweets)
+        document.body.append(ol)
+        
         // [3] créer un <bouton> de filtre pour que quand on clique dessus, ne garde que les tweets en français à l'écran
+        const olFrench = createOl(tweets.filter(tweet => {
+          return tweet.lang === 'fr'
+        }))
+        const buttonFrench = createFrenchButton('Français', 'click', ol, olFrench)
+
+        let tracking = false
+        const buttonTrack = document.createElement('button')
+        buttonTrack.textContent = 'Mouse tracking'
+        buttonTrack.addEventListener(
+          'click',
+          function () {
+            tracking = !tracking
+            if (tracking) {
+              document.addEventListener(
+                'mousemove',
+                listenerTrack
+              )
+            } else {
+              document.removeEventListener(
+                'mousemove',
+                listenerTrack
+              )
+            }
+          }
+        )
+
+        document.body.append(buttonFrench, buttonTrack)
 
         // [4] modifier le bouton de filtre pour pouvoir réafficher tous les tweets quand on reclique dessus
 
@@ -53,7 +132,7 @@ document.addEventListener(
       })
       .catch(function (e) {
         console.error(e);
-      });
-  },
-  { once: true },
+      })
+  // },
+  // { once: true },
 );
